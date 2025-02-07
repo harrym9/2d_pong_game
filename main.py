d42987db6632e10e62b6e3229c3cddbd4582826f
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-02-07 17:10:58 +0300

    Right paddle and control(up & down) added

--- main.py
+++ main.py
@@ -9,11 +9,23 @@ screen.bgcolor(BACKGROUND_COLOR)
 screen.setup(width=WIDTH, height=HEIGHT)
 screen.title("Pong Game")
 
+def up():
+    turtle.forward(15)
+def down():
+    turtle.backward(15)
+
+turtle = Turtle()
+turtle.color("white")
+turtle.shape("square")
+turtle.shapesize(stretch_len=4)
+turtle.setheading(90)
+turtle.penup()
+turtle.speed(0)
+turtle.goto(x=(WIDTH // 2) - 30, y=0)
 
 
-
-
-
+screen.onkey(fun=up, key="Up")
+screen.onkey(fun=down, key="Down")
 
 
 
