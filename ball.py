52a5fd28755dbdb2266cae0ec0d9f1d2445cdccb
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-02-11 16:16:18 +0300

    Detecting when paddle's misses the ball and restart it

--- ball.py
+++ ball.py
@@ -20,3 +20,7 @@ class Ball(Turtle):
 
     def bounce_x(self):
         self.x_move *= -1
+
+    def restart(self):
+        self.bounce_x()
+        self.goto(0,0)
--- main.py
+++ main.py
@@ -27,16 +27,23 @@ screen.onkey(fun=l_paddle.go_down, key="s".lower())
 
 is_game_on = True
 while is_game_on:
-    time.sleep(0.1)
+    time.sleep(0.06)
     ball.move()
     screen.update()
 
     # Detect collisions with top and bottom walls
-    if ball.ycor() >= ((HEIGHT / 2) - 20) or ball.ycor() <= -((HEIGHT / 2) + 20):
+    if ball.ycor() >= ((HEIGHT / 2) - 20) or ball.ycor() <= -((HEIGHT / 2) - 20):
         ball.bounce_y()
 
     # Detect collisions with left and right paddle's
-    if ball.xcor() >= ((WIDTH/2) - 50) and ball.distance(r_paddle) < 50 or ball.xcor() <= -((WIDTH/2) - 40) and ball.distance(l_paddle) < 50:
+    if ball.xcor() >= ((WIDTH / 2) - 50) and ball.distance(r_paddle) < 50 or ball.xcor() <= -(
+            (WIDTH / 2) - 40) and ball.distance(l_paddle) < 50:
         ball.bounce_x()
+
+        # Detect when paddle's misses the ball
+    if ball.xcor() >= 400:
+        ball.restart()
+    elif ball.xcor() <= -400:
+        ball.restart()
 
 screen.exitonclick()
